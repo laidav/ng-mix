abfa92c5c0a7765c3902f4c783110ac1eadde57c
David Lai <laidav@gmail.com>
2022-02-16 15:46:23 -0500

    formatting

--- projects/ng-mix/src/lib/models/BaseClassInjector.ts
+++ projects/ng-mix/src/lib/models/BaseClassInjector.ts
@@ -4,6 +4,7 @@ import { Injectable, Injector } from "@angular/core";
 	providedIn: 'root'
 })
 export class BaseClassInjector {
-	ngOnInit () {};
 	constructor(public injector: Injector) {}
+
+	ngOnInit () {};
 }
