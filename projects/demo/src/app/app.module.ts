e6ac90af300c469299ee22838af4b1e588ca4906
David Lai <laidav@gmail.com>
2022-03-16 13:27:29 -0400

    add initial components

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -5,16 +5,86 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { BeerCountComponent } from './beer-count/beer-count.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { TypeaheadComponent } from './typeahead/typeahead.component';
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatDividerModule } from '@angular/material/divider';
+
+const matModule = [
+  //TODO: browser module should be at root level of applicationBrowserAnimationsModule,
+  MatSidenavModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSlideToggleModule,
+  MatCardModule,
+  MatDialogModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatCardModule,
+  MatRadioModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatProgressSpinnerModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatTabsModule,
+  MatMenuModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatButtonToggleModule,
+  DragDropModule,
+  MatDividerModule,
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SandboxComponent,
-    BeerCountComponent
+    BeerCountComponent,
+    EmployeeListComponent,
+    TypeaheadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ...matModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ projects/demo/src/app/typeahead/typeahead.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { Option } from '../models/Option';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-typeahead',
+  templateUrl: './typeahead.component.html',
+  styleUrls: ['./typeahead.component.scss']
+})
+export class TypeaheadComponent implements OnInit {
+  @Input() options: Option<any>[] = [];
+  @Output() optionSelected = new EventEmitter<Option<any>>();
+
+  searchControl = new FormControl();
+
+  filteredOptions$: Observable<Option<any>[]> | null = null;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.filteredOptions$ = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(value: string): Option<any>[] {
+    return this.options.filter(option => option.label.toLowerCase().includes(value));
+  }
+}
