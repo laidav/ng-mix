3068cc859551abf225a8a34a396a1918b1602702
David Lai <laidav@gmail.com>
2022-03-21 09:55:44 -0400

    add deprecated comment to combine mixins

--- projects/ng-mix/src/lib/helpers/combineMixins.ts
+++ projects/ng-mix/src/lib/helpers/combineMixins.ts
@@ -1,1 +1,4 @@
+/**
+ * @deprecated The method should not be used
+ */
 export const composeMixins = (...functions: any): any => functions.reduce((a: any, b: any) => (...args: any) => a(b(...args)))();
