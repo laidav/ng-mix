659549882a3569e76539cd63f0f32e677bef9f96
David Lai <laidav@gmail.com>
2022-03-20 20:07:48 -0400

    fix unit tests

--- projects/demo/src/app/beer-count/beer-count.component.ts
+++ projects/demo/src/app/beer-count/beer-count.component.ts
@@ -2,9 +2,8 @@ import { Component, Injector } from '@angular/core';
 import { CounterMixin } from '../../mixins/counter.mixin';
 import { LabelMixin } from '../../mixins/label.mixin';
 import { Base } from 'ng-mix';
-import { PersonsMixin } from '../../mixins/persons.mixin';
 
-const mixins = PersonsMixin(LabelMixin(CounterMixin(Base)));
+const mixins = LabelMixin(CounterMixin(Base));
 
 @Component({
   selector: 'app-beer-count',
@@ -16,6 +15,4 @@ export class BeerCountComponent extends mixins {
   constructor(injector: Injector) {
     super(injector);
   }
-
-  ngOnInit() {}
 }
